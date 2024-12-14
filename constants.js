02c8a1e09e44bcd893133eb71b3634109629a353
Ranvir Singh <ranvir.singh@quantela.com>
2024-12-14 10:52:44 +0530

    initial project setup done

---
+++ constants.js
@@ -0,0 +1,3 @@
+const DB_NAME="phone_validator"
+
+export {DB_NAME}
---
+++ models/phonenumber.model.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+const phonenumberSchema = new mongoose.Schema();
+
+
+export const Phonenumber = new mongoose.Schema("Phonenumber", phonenumberSchema);
